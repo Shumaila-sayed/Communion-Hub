65895b0b2c38187bf57435d5b91ed9f8a1bd48e2
Shumaila <sayedshumaila27@gmail.com>
2025-03-17 03:35:26 +0530

    Add header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+ import '../styles/header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>CommunionHub</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Events</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
